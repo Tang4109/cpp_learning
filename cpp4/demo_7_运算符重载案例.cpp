4401c7df23dbcaa69523a000ede76212d125f747
Tang4109 <2298964704@qq.com>
2019-04-25 22:27:16 +0800

    运算符重载案例

---
+++ cpp4/demo_7_运算符重载案例.cpp
@@ -0,0 +1,20 @@
+/****************************************!
+*@brief  ÔËËã·ûÖØÔØ°¸Àı
+*@author ZhangYunjia
+*@date   2019/4/25/22:04
+
+****************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+
+	
+	cout<<"end.."<<endl;
+	system("pause");
+	return 0;
+}
