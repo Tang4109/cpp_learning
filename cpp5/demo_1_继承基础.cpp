43e061baf0a98b8b9587dee81be511a1b8404d76
Tang4109 <2298964704@qq.com>
2019-04-27 16:07:20 +0800

    继承的基础语法

--- cpp5/demo_1_继承基础.cpp
+++ cpp5/demo_1_继承基础.cpp
@@ -1,5 +1,5 @@
 /****************************************!
-*@brief  
+*@brief  ¼Ì³ÐµÄ»ù´¡Óï·¨
 *@author ZhangYunjia
 *@date   2019/4/27/15:13
 
@@ -9,12 +9,37 @@
 
 using namespace std;
 
+class parent
+{
+public:
+	void print() {
+		cout << a << endl << b << endl;
+	}
+protected:
+public:
+	int a;
+	int b;
+
+};
+
+class child:public parent
+{
+public:
+protected:
+private:
+	int c;
+}; 
+
+
+
 
 int main()
 {
 
+	parent p1;
+	child c1;
+	c1.a;
 	
-	cout << 1 << endl;
 	cout<<"end.."<<endl;
 	system("pause");
 	return 0;
