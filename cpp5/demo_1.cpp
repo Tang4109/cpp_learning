861fd2703be5fee8eab91020b19b4731dcfb5ca5
Tang4109 <2298964704@qq.com>
2019-04-27 15:15:46 +0800

    1

---
+++ cpp5/demo_1.cpp
@@ -0,0 +1,21 @@
+/****************************************!
+*@brief  
+*@author ZhangYunjia
+*@date   2019/4/27/15:13
+
+****************************************/
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	
+	
+	cout<<"end.."<<endl;
+	system("pause");
+	return 0;
+}
