0c1eac511210bbaeb77b0bf34710a25a162c4d48
Tang4109 <2298964704@qq.com>
2019-05-10 14:54:42 +0800

    搭建动态库（实现初始化/发送/接收/内存释放）

--- c/mysocketclient/mysocketclient/mysocketclient.c
+++ c/mysocketclient/mysocketclient/mysocketclient.c
@@ -9,33 +9,105 @@
 #include<string.h>
 #include<stdio.h>
 
+typedef struct _SCK_HANDLE {
+
+	char version[64];
+	char ip[128];
+	int port;
+	unsigned char* p;
+	int p_len;
+
+}SCK_HANDLE; //动态库内部的数据类型，不想让上层应用知道-数据类型的封装
+
 //客户端初始化 获取handle上下
 __declspec(dllexport)
 int cltSocketInit(void **handle /*out*/) {
-
+	int ret = 0;
 	printf("func cltSocketInit() begin.\n");
+	SCK_HANDLE* handle_ = NULL;
+	handle_ = (SCK_HANDLE*)malloc(sizeof(SCK_HANDLE));
+	if (handle_ ==NULL)
+	{
+		ret = -1;
+		printf("func cltSocketInit err:%d\n", ret);
+		return ret;
+	}
+	memset(handle_, 0, sizeof(SCK_HANDLE));
+	strcpy(handle_->ip, "192.168.1.1");
+	handle_->port = 8081;
+	*handle = handle_;
+
 	printf("func cltSocketInit() end.\n");
 
-	return 0;
+	return ret;
 }
 
 //客户端发报文
 __declspec(dllexport)
 int cltSocketSend(void *handle /*in*/, unsigned char *buf /*in*/, int buflen /*in*/) {
+	int ret = 0;
+	if (handle==NULL||buf==NULL)
+	{
+		ret = -2;
+		printf("func cltSocketSend err:%d\n", ret);
+		return ret;
+	}
 
-	return 0;
+	printf("func cltSocketSend() begin.\n");
+	SCK_HANDLE* handle_ = NULL;
+	handle_ = (SCK_HANDLE*)handle;
+	handle_->p = (unsigned char*)malloc(buflen * sizeof(unsigned char));
+	if (handle_->p == NULL) {
+		ret = -3;
+		printf("func cltSocketSend err:%d\n", ret);
+		return ret;
+	}
+	memcpy(handle_->p, buf, buflen);
+	handle_->p_len = buflen;
+	printf("func cltSocketSend() end.\n");
+	return ret;
 }
 
 //客户端收报文
 __declspec(dllexport)
-int cltSocketRev(void *handle /*in*/, unsigned char *buf /*in*/, int *buflen /*in out*/) {
+int cltSocketRev(void *handle /*in*/, unsigned char *out_buf /*in*/, int *outbuflen /*in out*/) {
+	int ret = 0;
+	if (handle == NULL || out_buf == NULL|| outbuflen ==NULL)
+	{
+		ret = -4;
+		printf("func cltSocketRev err:%d\n", ret);
+		return ret;
+	}
 
-	return 0;
+	printf("func cltSocketRev() begin.\n");
+	SCK_HANDLE* handle_ = NULL;
+	handle_ = (SCK_HANDLE*)handle;
+	memcpy(out_buf, handle_->p, handle_->p_len);
+	*outbuflen = handle_->p_len;
+	printf("func cltSocketRev() end.\n");
+	return ret;
 }
 
 //客户端释放资源
 __declspec(dllexport)
 int cltSocketDestory(void *handle/*in*/) {
 
-	return 0;
+	int ret = 0;
+	if (handle == NULL)
+	{
+		ret = -5;
+		printf("func cltSocketDestory err:%d\n", ret);
+		return ret;
+	}
+
+	printf("func cltSocketDestory() begin.\n");
+	SCK_HANDLE* handle_ = NULL;
+	handle_ = (SCK_HANDLE*)handle;
+	if (handle_->p) {
+		free(handle_->p);
+
+	}
+	free(handle_);
+	printf("func cltSocketDestory() end.\n");
+	return ret;
 }
--- c/搭建动态库/搭建动态库/测试框架.c
+++ c/搭建动态库/搭建动态库/测试框架.c
@@ -1,8 +1,7 @@
 /****************************************!
-*@brief  搭建动态库
+*@brief  搭建动态库（实现初始化/发送/接收/内存释放）
 *@author ZhangYunjia
 *@date   2019/5/10/10:37
-
 ****************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdlib.h>
@@ -17,20 +16,20 @@ int main()
 	void *handle = NULL;
 	char buf[128];
 	int buf_len = 3;
-	strcpy(buf, "zzzzzzzzzzzzzzz");
+	strcpy(buf, "zzz");
 
 	char out_buf[128];
 	int out_buf_len = 3;
 
 	ret = cltSocketInit(&handle /*out*/);
-	if (ret!=0)
+	if (ret != 0)
 	{
 		printf("func cltSocketInit() err: %d\n", ret);
 		return ret;
 	}
 
 	//客户端发报文
-	ret = cltSocketSend(handle /*in*/, buf /*in*/, &buf_len /*in*/);
+	ret = cltSocketSend(handle /*in*/, buf /*in*/, buf_len /*in*/);
 	if (ret != 0)
 	{
 		printf("func cltSocketInit() err: %d\n", ret);
@@ -48,10 +47,6 @@ int main()
 	//客户端释放资源
 	cltSocketDestory(handle/*in*/);
 
-
-
-
-	
 	printf("end...\n");
 	system("pause");
 	return 0;
