478eb2c2f6bc60ff107f44452c96acc7e78e1ef1
Tang4109 <2298964704@qq.com>
2019-05-17 14:00:05 +0800

    数组模板类框架

--- 泛型编程/泛型编程/my_vector.cpp
+++ 泛型编程/泛型编程/my_vector.cpp
@@ -12,6 +12,11 @@ using namespace std;
 template <typename T>
 ostream& operator<<(ostream &out, my_vector<T> &obj)
 {
+	for (int i=0;i<obj.m_len;i++)
+	{
+		out << obj.m_space[i] << " ";
+	}
+	out << endl;
 	return out;
 }
 
@@ -19,32 +24,60 @@ ostream& operator<<(ostream &out, my_vector<T> &obj)
 template <typename T>
 my_vector<T>::my_vector(int size)//构造函数
 {
+	m_space = new T[size];
+	m_len = size;
 
 }
 template <typename T>
 my_vector<T>::my_vector(const my_vector &obj)//copy构造函数
 {
-
+	//根据my_v1的大小分配内存
+	m_len = obj.m_len;
+	m_space = new T[m_len];
+	//copy数据
+	for (int i=0;i<m_len;i++)
+	{
+		m_space[i] = obj.m_space[i];
+	}
 }
 
 template <typename T>
 my_vector<T>::~my_vector()//析构函数
 {
-
+	if (m_space!=NULL)
+	{
+		delete[] m_space;
+		m_space = NULL;
+		m_len = 0;
+	}
 
 }
 template <typename T>
 T& my_vector<T>::operator[](int index)
 {
-
 	return m_space[index];
 }
 
 template <typename T>
 my_vector<T>& my_vector<T>::operator=(my_vector<T> &obj)
 {
+	//先把a2的旧的内存释放掉
+	if (m_space!=NULL)
+	{
+		delete[] m_space;
+		m_space = NULL;
+		m_len = 0;
+	}
+	//根据a1分配内存
+	m_len = obj.m_len;
+	m_space = new T[m_len];
+	//copy数据
+	for (int i = 0; i < m_len; i++)
+	{
+		m_space[i] = obj.m_space[i];
+	}
 
-	return *this;
+	return *this;//a2=a1.返回a2的自身
 }
 
 template <typename T>
--- 泛型编程/泛型编程/my_vector_test.cpp
+++ 泛型编程/泛型编程/my_vector_test.cpp
@@ -1,5 +1,5 @@
 /****************************************!
-*@brief  
+*@brief  数组模板类框架
 *@author ZhangYunjia
 *@date   2019/5/17/10:10
 
@@ -13,20 +13,25 @@ using namespace std;
 int main()
 {
 	my_vector<int> my_v1(10);
-	for (int i = 0;i<my_v1.get_len();i++)
+	int len = my_v1.get_len();
+	for (int i = 0;i< len;i++)
 	{
 		my_v1[i] = i;
 		cout << my_v1[i] << " ";
 	}
 	cout << endl;
 	my_vector<int>my_v2 = my_v1;
-	for (int i = 0; i < my_v1.get_len(); i++)
+	
+	for (int i = 0; i < len; i++)
 	{
 		cout << my_v2[i] << " ";
 	}
 
+	my_vector<int>my_v3;
+	my_v3 = my_v1;
+
 	//重载左移运算符
-	cout << my_v2 << endl;
+	cout << my_v3 << endl;
 
 	cout<<"\nend.."<<endl;
 	system("pause");
