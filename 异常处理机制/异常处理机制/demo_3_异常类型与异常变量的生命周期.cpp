54e675ae6ba09ce925f4b2fe18f4c042d640a413
Tang4109 <2298964704@qq.com>
2019-05-18 19:40:41 +0800

    异常

---
+++ 异常处理机制/异常处理机制/demo_3_异常类型与异常变量的生命周期.cpp
@@ -0,0 +1,38 @@
+/****************************************!
+*@brief  
+*@author ZhangYunjia
+*@date   2019/5/18/19:32
+
+****************************************/
+
+#include<iostream>
+
+using namespace std;
+
+void my_strcpy(char* to, char* from) {
+
+
+}
+
+
+int main()
+{
+	try
+	{
+	}
+	catch (CMemoryException* e)
+	{
+		
+	}
+	catch (CFileException* e)
+	{
+	}
+	catch (CException* e)
+	{
+	}
+	
+	
+	cout<<"end.."<<endl;
+	system("pause");
+	return 0;
+}
