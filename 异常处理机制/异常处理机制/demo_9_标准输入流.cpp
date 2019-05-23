b5f22eedf1f7665dcdd5d11b41c5737fc4342e78
Tang4109 <2298964704@qq.com>
2019-05-23 08:39:29 +0800

    cin.get/cin.getline

--- 异常处理机制/异常处理机制/demo_9_标准输入流.cpp
+++ 异常处理机制/异常处理机制/demo_9_标准输入流.cpp
@@ -1,5 +1,5 @@
 /****************************************!
-*@brief  标准输入流
+*@brief  cin.get/cin.getline
 *@author ZhangYunjia
 *@date   2019/5/22/20:24
 
@@ -10,9 +10,9 @@
 using namespace std;
 
 
-int main()
+int main91()
 {
-
+	//cin.get一次只能读取一个字符
 	char ch;
 	while ((ch=cin.get())!=EOF)
 	{
@@ -20,6 +20,39 @@ int main()
 	}
 	
 	cout<<"end.."<<endl;
+	system("pause");
+	return 0;
+}
+int main92()   /* 2019/5/23/8:23 */
+{
+
+	//cin.get(一个参数) //读一个字符
+	char a, b, c;
+	cin.get(a);
+	cin.get(b);
+	cin.get(c);
+	//缓冲区有数据，程序不会阻塞
+	cout << a << b << c << endl;
+	//链式编程
+	cin.get(a).get(b).get(c);
+	cout << a << b << c << endl;
+
+	system("pause");
+	return 0;
+}
+int main()   /* 2019/5/23/8:30 */
+{
+
+	//cin.getline() 可以接受空格
+	char buf1[256];
+	char buf2[256];
+	cout << "请输入一个含有多个空格的字符串：" << endl;
+
+	cin >> buf1;
+	cin.getline(buf2, 256);
+
+	cout << "buf1:" << buf1 << "buf2:" << buf2 << endl;
+	
 	system("pause");
 	return 0;
 }
