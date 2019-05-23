1ea8257974273a7d1cc4626c45c0381dd6ca6b23
Tang4109 <2298964704@qq.com>
2019-05-23 09:15:50 +0800

    cin.get/cin.getline/cin.ignore/ cin.peek/cin.putback

--- 异常处理机制/异常处理机制/demo_9_标准输入流.cpp
+++ 异常处理机制/异常处理机制/demo_9_标准输入流.cpp
@@ -1,12 +1,12 @@
 /****************************************!
-*@brief  cin.get/cin.getline
+*@brief  cin.get/cin.getline/cin.ignore/ cin.peek/cin.putback
 *@author ZhangYunjia
 *@date   2019/5/22/20:24
 
 ****************************************/
 
 #include<iostream>
-
+#include<string>
 using namespace std;
 
 
@@ -40,7 +40,7 @@ int main92()   /* 2019/5/23/8:23 */
 	system("pause");
 	return 0;
 }
-int main()   /* 2019/5/23/8:30 */
+int main93()   /* 2019/5/23/8:30 */
 {
 
 	//cin.getline() 可以接受空格
@@ -56,3 +56,47 @@ int main()   /* 2019/5/23/8:30 */
 	system("pause");
 	return 0;
 }
+
+int main94()   /* 2019/5/23/8:41 */
+{
+
+	char buf1[256];
+	char buf2[256];
+	cout << "请输入一个含有多个空格的字符串：" << endl;
+
+	cin >> buf1;
+	cin.ignore(2);//忽略两个空格
+	int my_int = cin.peek();//返回缓冲区的第一个元素
+	cout << "my_int: " << my_int << endl;
+	cin.getline(buf2, 256);
+
+	cout << "buf1:" << buf1 << "buf2:" << buf2 << endl;
+
+	system("pause");
+	return 0;
+}
+
+int main()   /* 2019/5/23/9:02 */
+{
+	char c = cin.get();
+	if ((c >= '0') && (c <= '9')) //输入的整数和字符串 分开处理
+	{
+		int n; //整数不可能 中间有空格 使用cin >>n
+		cin.putback(c);
+		cin >> n;
+		cout << "You entered a number: " << n << '\n';
+	}
+	else
+	{
+		string str;
+		cin.putback(c);
+		getline(cin, str); // //字符串 中间可能有空格 使用 cin.getline();
+		cout << "You entered a word: " << str << '\n';
+	}
+
+
+	system("pause");
+	return 0;
+}
+
+
