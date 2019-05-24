ad174e80295e1ec97be053afd3d1037bff12dc22
Tang4109 <2298964704@qq.com>
2019-05-24 13:48:57 +0800

    string

--- STL/STL/demo_2_string.cpp
+++ STL/STL/demo_2_string.cpp
@@ -4,7 +4,7 @@
 *@date   2019/5/24/8:56
 
 ****************************************/
-
+#define _SCL_SECURE_NO_WARNINGS
 #include<iostream>
 #include "string"
 using namespace std;
@@ -39,15 +39,85 @@ void string_traverse() {
 	{
 		cout << &it << " ";
 	}
+	cout << endl;
+	//3.
+	try
+	{
+		for (int i = 0; i < s.length()+3; i++)
+		{
+			cout << s.at(i) << " ";//可以抛出异常
+		}
+	}
+	catch (...)
+	{
+		cout << "发生异常" << endl;
+	}
+
+
+}
+
+//字符指针和string类的转换
+void string_transfer() {
+	//1.char* -> string
+	string s = "zyj";
+	//2.string -> char*
+	cout << s.c_str() << endl;
+	printf("%s", s.c_str());
+	cout << endl;
+	//3.s的内容copy到buf中
+	char buf[128] = { 0 };
+	s.copy(buf,2,1);
+	cout << buf << endl;
+
+}
+//字符串的连接
+void string_connect() {
+	string s1 = "zyjzyjzyzjyzj";
+	string s2 = "11111233333";
+	string s = s1 + s2;
+	cout << s << endl;
+	s.append(s1);
+	cout << s << endl;
+
+}
+
+//字符串的查找和替换
+void string_search() {
+	string s = "hello zyj hello you are zyj  a big  zyj boy zyj.";
+	//zyj 第一次出现的位置
+	int index = s.find("zyj", 0);
+	cout << index << endl;
+	//zyj出现的次数及每一次出现的数组下标
+	int num = 0;
+	while (index!=string::npos)
+	{
+		num++;
+		cout << "index:" << index << " ";
+		s.replace(index, 3, "ZYJ");//替换
+		index++;
+		index = s.find("zyj", index);
+	}
+	cout << endl;
+	cout << "num:" << num << endl;
+	//替换
+	cout << s << endl;
+
 }
 
 int main()
 {
-
+	
 	string_init();
 	string_traverse();
-	
-	cout<<"end.."<<endl;
+	cout << "*************************1" << endl;
+	string_transfer();
+	cout << "*************************2" << endl;
+	string_connect();
+	cout << "*************************3" << endl;
+	string_search();
+
+	cout<<"\nend.."<<endl;
 	system("pause");
 	return 0;
 }
+
