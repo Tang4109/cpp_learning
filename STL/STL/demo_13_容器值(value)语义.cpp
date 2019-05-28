f62f22f2649c946a0b0db7d4431871ab2288ce1a
Tang4109 <2298964704@qq.com>
2019-05-28 08:22:36 +0800

    容器值(value)语义

--- STL/STL/demo_13_容器值(value)语义.cpp
+++ STL/STL/demo_13_容器值(value)语义.cpp
@@ -1,88 +1,90 @@
 /****************************************!
-*@brief  容器的值拷贝语义
+*@brief  容器值(value)语义
 *@author ZhangYunjia
 *@date   2019/5/27/15:52
 
 ****************************************/
-
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
-using namespace std;
+#include<iostream>
 #include "vector"
+using namespace std;
+
 
 class Teacher
 {
 public:
-	Teacher(const char *name, int age)
+	Teacher(const char* name_, int age_)
 	{
-		m_pname = new char[strlen(name) + 1];
-		strcpy(m_pname, name);
-		m_age = age;
+		age = age_;
+		name = new char[strlen(name_)+1];
+		strcpy(name, name_);
+		//name = name_;
 	}
+	//copy构造函数,实现功能Teacher t2=t1;
+	Teacher(const Teacher& obj)
+	{
+		name = new char[strlen(obj.name) + 1];
+		strcpy(name, obj.name);
+		age = obj.age;
+	}
+
 	~Teacher()
 	{
-		if (m_pname != NULL)
+		if (name!=NULL)
 		{
-			delete[] m_pname;
-			m_pname = NULL;
-			m_age = 0;
-		}
-	}
-
-	//Teacher t2 = t1;
-	Teacher(const Teacher &obj)
-	{
-		m_pname = new char[strlen(obj.m_pname) + 1];
-		strcpy(m_pname, obj.m_pname);
-		m_age = obj.m_age;
-	}
-
-	//重载 =号操作符
-	//t3 = t2 = t1
-	Teacher& operator=(const Teacher &obj)
-	{
-		//先把t2的旧的内存释放掉
-		if (m_pname != NULL)
-		{
-			delete[] m_pname;
-			m_pname = NULL;
-			m_age = 0;
+			delete[] name;
+			name = NULL;
+			age = 0;
 		}
 
-		//根据t1的大小分配内存 
-		m_pname = new char[strlen(obj.m_pname) + 1];
-		//copy t1的数据
-		strcpy(m_pname, obj.m_pname);
-		m_age = obj.m_age;
+	}
+
+	void print()
+	{
+		cout << name << ":" << age << endl;
+
+	}
+	//重载等号操作符：t3=t2=t1
+	Teacher & operator=(const Teacher &obj)
+	{
+		//1.先把t2的旧内存释放掉
+		if (name!=NULL)
+		{
+			delete[] name;
+			name = NULL;
+			age = 0;
+		}
+
+		//2.根据t1的大小分配内存
+		name = new char[strlen(obj.name) + 1];
+
+		//3.copy t1的数据
+		strcpy(name, obj.name);
+		age = obj.age;
 		return *this;
 	}
 
-public:
 
-	void printT()
-	{
-		cout << m_pname << "\t" << m_age << endl;
-	}
-protected:
 private:
-	char	*m_pname;
-	int		m_age;
-};
+	char* name;
+	int age;
+ };
 
 void test()
 {
 	Teacher t1("t1", 31);
-	t1.printT();
-
+	t1.print();
 	vector<Teacher> v1;
-	v1.push_back(t1); //把t1拷贝了一份 存入到容器中了....
+	v1.push_back(t1);//把t1拷贝一份放在容器中
 
 }
-void main()
+
+int main()
 {
+
 	test();
-	cout << "hello..." << endl;
+	
+	cout<<"end.."<<endl;
 	system("pause");
-	return;
+	return 0;
 }
